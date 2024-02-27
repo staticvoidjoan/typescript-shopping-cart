0983d94b8f4528d410f5c64f4bd270a3d5c49d56
Joan <104306249+staticvoidjoan@users.noreply.github.com>
2024-02-27 10:54:30 +0100

    first commit

---
+++ src/pages/Store.tsx
@@ -0,0 +1,15 @@
+import { StoreItem } from "../components/StoreItem";
+import storeItems from "../data/items.json";
+import { Col, Row } from "react-bootstrap";
+
+export default function Store() {
+  return (
+    <div>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}><StoreItem {...item}/></Col>
+        ))}
+      </Row>
+    </div>
+  );
+}
